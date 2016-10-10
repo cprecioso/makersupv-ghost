97333bb2477c394816361caefda26d3fd6ecd817
Carlos Precioso <kalprestito@gmail.com>
2016-10-11 01:02:41 +0200

    Set path

--- config.js
+++ config.js
@@ -24,6 +24,10 @@ config = {
         server: {
             host: '127.0.0.1',
             port: '2368'
+        },
+        
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
         }
     },
 
